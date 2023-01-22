415fe5ae5de3ebd184cec15b1f2f231c75dd795f
Brian Falasz <bfalasz@yahoo.com>
2023-01-21 19:10:08 -0600

    adding beginning of my new react project. First one written in typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App(): any {
+  return (
+    <div className="App">
+      <h1>Flip Cards</h1>
+      
+    </div>
+  );
+}
+
+export default App;
