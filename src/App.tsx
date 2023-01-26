5cee184c8798f7f9e794ee5adffe22dac48865a6
Brian Falasz <bfalasz@yahoo.com>
2023-01-25 20:23:19 -0600

    now able to make a list of collections

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,39 @@ import Collections from './components/Collections';
 
 function App(): any {
 
-  const [collectionName, setCollectionName] = React.useState<string>('Start a New Collection...');
-
+  const [collections, setCollections] = React.useState<object>({});
+  const [collectionName, setCollectionName] = React.useState<string>('Start New Collection...');
+  const [currentCollection, setCurrentCollection] = React.useState<object[]>([]);
   const createCollection = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
 
-    setCollectionName(e.target.value)
-
+    setCollectionName(e.target.value);
   }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      // Update state when enter key is pressed
+
+
+      setCollections({...collections, [collectionName]: []});
+    }
+  }
+  
 
   return (
     <div className="App">
       <h1>Flash Cards</h1>
-      <Collections createCollection={createCollection} collectionName={collectionName} />
-      <Collection />
+      <div className="collections-row">
+        <Collections 
+          createCollection={createCollection} 
+          collectionName={collectionName} 
+          handleAddCollection={handleKeyDown} 
+          collections={collections}
+        />
+      </div>
+      <div className="collection-view"> 
+        <Collection />
+      </div>
     </div>
   );
 }
--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 
 interface CollectionsProps {
   createCollection: React.ChangeEventHandler<HTMLInputElement>,
+  handleAddCollection: React.KeyboardEventHandler<HTMLInputElement>,
   collectionName: string,
+  collections: object,
 }
 
-const Collections = ({createCollection, collectionName}: CollectionsProps) => {
-
-  const [collections, setCollections] = React.useState<object[]>([])
-
+const Collections = ({createCollection, handleAddCollection, collectionName, collections}: CollectionsProps) => {
+  
+  const collectionList: string[] = Object.keys(collections);
 
   return (
     <div>
-      <p>Collections</p>
+      <p><strong>Collections</strong></p>
       <ul>
+        {collectionList.map(collection => {
+          return <li>{collection}</li>
+        })}
       </ul>
-      <input onChange={createCollection} value={collectionName}/>
+      <input onChange={createCollection} onKeyDown={handleAddCollection} value={collectionName}/>
     </div>
   )
 
