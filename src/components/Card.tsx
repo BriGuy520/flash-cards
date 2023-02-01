e3bc6edc65dce5e34e87ce0241464ee1c153b9e3
Brian Falasz <bfalasz@yahoo.com>
2023-01-31 21:09:28 -0600

    adding an edit button icon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponentElement } from 'react';
 import { CardDetails } from "./Collection";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 
 type CardProps = {
@@ -23,7 +25,8 @@ const Card = ({card, handleDeleteCard}: CardProps) => {
   return (
     <div className="card m-3 col-lg-3" onClick={handleCardClick}>
       <div className="flip-card-inner">
-        <button onClick={() => handleDeleteCard(card.id)}>X</button>
+        <button className="edit-icon" onClick={() => console.log("edit card")}><FontAwesomeIcon icon={faPenToSquare} /></button>
+        <button className="delete-icon" onClick={() => handleDeleteCard(card.id)}><FontAwesomeIcon icon={faTrash} /></button>
         <div style={{display: showQuestion ? 'block' : 'none'}} className="card-front card-body">
           <h2>{card.question}</h2>
 
