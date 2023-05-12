6710731a2c6a60b20be170e42adc1f36935443d8
Brian Falaz <brianfalasz@gmail.com>
2023-05-12 16:14:08 -0500

    beginning of update to the collections data to incorporate card practice state to fix an issue with the shuffle feature

--- src/db/data.js
+++ src/db/data.js
@@ -3,51 +3,60 @@ export const data = {
         {
             "id": 1675634588252,
             "question": "what is react?",
-            "answer": "A frontend Framework for building web applications"
+            "answer": "A frontend Framework for building web applications",
+            "practiced": null, 
         },
         {
             "id": 1675635150392,
             "question": "who maintains react?",
-            "answer": "Meta maintains and develops React."
+            "answer": "Meta maintains and develops React.",
+            "practiced": null, 
         },
         {
             "id": 1675635152025,
             "question": "What does React do?",
-            "answer": "React allows developers to build reusable UI components and manage the state of their application in an efficient and organized way"
+            "answer": "React allows developers to build reusable UI components and manage the state of their application in an efficient and organized way",
+            "practiced": null,
         },
         {
             "id": 1675635231494,
             "question": "how does react update the view?",
-            "answer": "It uses a virtual DOM (Document Object Model)"
+            "answer": "It uses a virtual DOM (Document Object Model)",
+            "practiced": null, 
         },
         {
             "id": 1675635267237,
             "question": "What is the virtual DOM?",
-            "answer": "The virtual DOM, or virtual Document Object Model, is a representation of the actual DOM in memory that is used by libraries such as React to efficiently update the user interface of web applications"
+            "answer": "The virtual DOM, or virtual Document Object Model, is a representation of the actual DOM in memory that is used by libraries such as React to efficiently update the user interface of web applications",
+            "practiced": null, 
         }
     ],
     "angular": [
         {
             "id": 1675635308872,
             "question": "what is angular?",
-            "answer": "Angular is a JavaScript framework for building web applications"
+            "answer": "Angular is a JavaScript framework for building web applications",
+            "practiced": null,
         },
         {
             "id": 1675635323582,
             "question": "who maintains angular?",
-            "answer": "It is developed and maintained by Google"
+            "answer": "It is developed and maintained by Google",
+            "practiced": null, 
         },
         {
             "id": 1675635366272,
             "question": "How Does angular work?",
-            "answer": "Angular uses a component-based architecture, which allows developers to build reusable UI components"
+            "answer": "Angular uses a component-based architecture, which allows developers to build reusable UI components",
+            "practiced": null,
         }
     ],
     "data Structures": [
         {
             "id": 1675635418450,
             "question": "what are the most important data structures to know",
-            "answer": "Arrays: A linear data structure that stores elements at contiguous memory locations.\n\nLinked Lists: A linear data structure that stores elements in nodes, with each node pointing to the next node.\n\nStacks: A linear data structure that follows the Last In First Out (LIFO) principle.\n\nQueues: A linear data structure that follows the First In First Out (FIFO) principle.\n\nTrees: A hierarchical data structure that consists of nodes, with each node having zero or more child nodes."
+            "answer": "Arrays: A linear data structure that stores elements at contiguous memory locations.\n\nLinked Lists: A linear data structure that stores elements in nodes, with each node pointing to the next node.\n\nStacks: A linear data structure that follows the Last In First Out (LIFO) principle.\n\nQueues: A linear data structure that follows the First In First Out (FIFO) principle.\n\nTrees: A hierarchical data structure that consists of nodes, with each node having zero or more child nodes.",
+            "practiced": null, 
         }
     ]
 }
